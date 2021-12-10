dfd85b90d27f6fa505e58ac5515d7f6baa9db25e
GitTher <estherfernandezflorez@gmail.com>
2021-12-10 13:07:00 +0100

    add collapsable headers

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('>> Ready :)');
+/* --- Función colapsar titulos formularios ---*/
+function handlerClickCollapsable(event) {
+  event.currentTarget.parentNode.classList.toggle('collapsed');
+}
+
+/* --- Collapsable Design --- */
+const collapsableDesign = document.querySelector('.js-design__title');
+collapsableDesign.addEventListener('click', handlerClickCollapsable);
+
+/* --- Collapsable Fill --- */
+const collapsableFill = document.querySelector('.js-fill__title');
+collapsableFill.addEventListener('click', handlerClickCollapsable);
+
+
+/* --- Collapsable Share --- */
+const collapsableShare = document.querySelector('.js-share__title');
+collapsableShare.addEventListener('click', handlerClickCollapsable);
+
+//console.log(formShare);
