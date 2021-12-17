e9c611496f7c936baa47e1f52b975b43ce2815b5
denisetroglio <overbeck.denise@gmail.com>
2021-12-17 14:31:13 +0100

    add JS colors change

--- src/js/02_form.js
+++ src/js/02_form.js
@@ -1,58 +1,99 @@
-'use strict';
+"use strict";
 
 function handleWriteInput(event) {
   const userInput = event.target.name;
   const userValue = event.target.value;
 
-  if (userInput === 'name') {
+  if (userInput === "name") {
     data.name = userValue;
-  } else if (userInput === 'job') {
+  } else if (userInput === "job") {
     data.job = userValue;
-  } else if (userInput === 'email') {
+  } else if (userInput === "email") {
     data.email = userValue;
-  } else if (userInput === 'phone') {
+  } else if (userInput === "phone") {
     data.phone = userValue;
-  } else if (userInput === 'linkedin') {
+  } else if (userInput === "linkedin") {
     data.linkedin = userValue;
-  } else if (userInput === 'github') {
+  } else if (userInput === "github") {
     data.github = userValue;
   }
   renderPreview();
 }
 
 function renderPreview() {
-  if (data.name === '') {
-    namePreview.innerHTML = 'Nombre Apellido';
+  if (data.name === "") {
+    namePreview.innerHTML = "Nombre Apellido";
   } else {
     namePreview.innerHTML = data.name;
   }
-  if (data.job === '') {
-    jobPreview.innerHTML = 'Front-End developer';
+  if (data.job === "") {
+    jobPreview.innerHTML = "Front-End developer";
   } else {
     jobPreview.innerHTML = data.job;
   }
-  if (data.email === '') {
-    emailPreview.href = '';
+  if (data.email === "") {
+    emailPreview.href = "";
   } else {
     emailPreview.href = `mailto:${data.email}`;
   }
-  if (data.linkedin === '') {
-    linkedinPreview.href = '';
+  if (data.linkedin === "") {
+    linkedinPreview.href = "";
   } else {
     linkedinPreview.href = `https://www.linkedin.com/in/${data.linkedin}`;
   }
-  if (data.github === '') {
-    githubPreview.href = '';
+  if (data.github === "") {
+    githubPreview.href = "";
   } else {
     githubPreview.href = `https://github.com/${data.github}`;
   }
-  if (data.phone === '') {
-    phonePreview.href = '';
+  if (data.phone === "") {
+    phonePreview.href = "";
   } else {
     phonePreview.href = `tel:${data.phone}`;
   }
 }
 
 for (const eachInput of allInput) {
-  eachInput.addEventListener('keyup', handleWriteInput);
+  eachInput.addEventListener("keyup", handleWriteInput);
+}
+
+// COLORES
+//Declarar variables:
+
+const inputColors = document.querySelectorAll(".js-palette");
+
+//Funciones:
+
+function handleColorChange() {
+  //const userInput = event.target.name;
+  //const userValue = event.currentTarget.value;
+
+  const inputRadio = document.querySelectorAll(".js-radio");
+  for (const eachRadio of inputRadio) {
+    if (eachRadio.value === "1") {
+      console.log("Holaaa");
+
+      namePreview.classList.add("colorgreen");
+      namePreview.classList.remove("colorred");
+      namePreview.classList.remove("coloryellow");
+    } else if (eachRadio.value === "2") {
+      console.log("Buenas");
+
+      namePreview.classList.remove("colorgreen");
+      namePreview.classList.add("colorred");
+      namePreview.classList.remove("coloryellow");
+    } else if (eachRadio.value === "3") {
+      console.log("Tchau");
+
+      namePreview.classList.remove("colorgreen");
+      namePreview.classList.remove("colorred");
+      namePreview.classList.add("coloryellow");
+    }
+  }
+}
+
+//Hacer un evento:
+
+for (const eachColors of inputColors) {
+  eachColors.addEventListener("click", handleColorChange);
 }
