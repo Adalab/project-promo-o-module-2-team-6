1563b444b249b16f26ce2863a4e6de12b4c24899
GitTher <estherfernandezflorez@gmail.com>
2022-01-07 12:54:52 +0100

    add create card button disabled after creating a card

--- src/js/04_card_url.js
+++ src/js/04_card_url.js
@@ -35,6 +35,7 @@ function handleCreateCard(event) {
       createLink.innerHTML = `<h3 class="form__card--title">La tarjeta ha sido creada:</h3> <a class="form__card--link" href="${data.cardURL}"target="_blank" title="URL card">${data.cardURL}</a>`;
       createButtonTw.innerHTML = `<a class="form__twitter--link" href="https://twitter.com/intent/tweet?text=¡Hola!%20Mira%20mi%20tarjeta%20de%20presentación%20de%20Awesome%20Cards%20&url=${data.cardURL}" target="_blank" title="twitter"><i class="form__twitter--icon fab fa-twitter"></i>Compartir en twitter</a>`;
     });
+  createButton.disabled = true;
 }
 
 createButton.addEventListener('click', handleCreateCard);
